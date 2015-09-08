40a74d3b5ff27c7a88dc69713bc49b11dd2bbcca
mulder0618 <138111370@qq.com>
2015-09-08 17:24:55 +0800

    activemq队列

--- src/main/java/com/zhx/base/queue/queuereceiver/QueueSendMailReceiver.java
+++ src/main/java/com/zhx/base/queue/queuereceiver/QueueSendMailReceiver.java
@@ -1,8 +1,11 @@
 package com.zhx.base.queue.queuereceiver;
 
 
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+import java.util.HashMap;
 
 /**
  * Created by admin on 2015/7/29.
@@ -11,6 +14,14 @@ public class QueueSendMailReceiver implements MessageListener {
 
 
     public void onMessage(Message message) {
-
+        HashMap object = null;
+        try {
+            object = (HashMap)((ObjectMessage) message).getObject();
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+        String title = String.valueOf(object.get("title"));
+        String email = String.valueOf(object.get("email"));
+        String content = String.valueOf(object.get("content"));
     }
 }
--- src/main/java/com/zhx/test/controller/TestController.java
+++ src/main/java/com/zhx/test/controller/TestController.java
@@ -1,11 +1,14 @@
 package com.zhx.test.controller;
 
+import com.zhx.base.queue.queuesender.QueueSender;
 import com.zhx.test.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
 
 /**
  * Created by mulder on 2015/6/16.
@@ -16,6 +19,9 @@ public class TestController {
 
     @Autowired
     private TestService testService;
+
+    @Autowired
+    private QueueSender queueSender;
 
     /**
      * ≤‚ ‘“≥√Ê
@@ -51,6 +57,20 @@ public class TestController {
     public ModelAndView testinsert(ModelAndView modelAndView){
         testService.testInsert("≤‚ ‘");
         testService.testInsert("≤‚1231222222sdfdsfsdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffsdfs ‘");
+        modelAndView.setViewName("test/testindex");
+        return modelAndView;
+    }
+
+    /**
+     * ≤‚ ‘∂”¡–
+      * @param modelAndView
+     * @return
+     */
+    @RequestMapping("/testqueue")
+    public ModelAndView testqueue(ModelAndView modelAndView){
+        HashMap param = new HashMap();
+        param.put("msg", "≤‚ ‘");
+        queueSender.send("test.queue", param);
         modelAndView.setViewName("test/testindex");
         return modelAndView;
     }
